447ea735efa9191a8395010c12f4b226e8d38c27
berdj2 <91245358+berdj2@users.noreply.github.com>
2024-01-18 11:55:30 +0100

    Add files via upload

---
+++ storage/framework/views/47a841825ac1be30aa80ca6a61d28cdf.php
@@ -0,0 +1,34 @@
+
+  
+<?php $__env->startSection('title', 'Create Voter'); ?>
+  
+<?php $__env->startSection('contents'); ?>
+    <h1 class="mb-0">Add Voter</h1>
+    <hr />
+    <form action="<?php echo e(route('Voters.store')); ?>" method="POST" enctype="multipart/form-data">
+        <?php echo csrf_field(); ?>
+        <div class="row mb-3">
+            <div class="col">
+                <input type="text" name="title" class="form-control" placeholder="Title">
+            </div>
+            <div class="col">
+                <input type="text" name="price" class="form-control" placeholder="Price">
+            </div>
+        </div>
+        <div class="row mb-3">
+            <div class="col">
+                <input type="text" name="Voter_code" class="form-control" placeholder="Voter Code">
+            </div>
+            <div class="col">
+                <textarea class="form-control" name="description" placeholder="Descriptoin"></textarea>
+            </div>
+        </div>
+ 
+        <div class="row">
+            <div class="d-grid">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+    </form>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\bergs\Ivote\resources\views/Voter/create.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/c7fa9034fab0db2faec2d7e5c44dc74e.php
@@ -0,0 +1,39 @@
+
+  
+<?php $__env->startSection('title', 'Show Voter'); ?>
+  
+<?php $__env->startSection('contents'); ?>
+    <h1 class="mb-0">Detail Voter</h1>
+    <hr />
+    <div class="row">
+        <div class="col mb-3">
+            <label class="form-label">candidate</label>
+            <input type="text" name="candidate" class="form-control" placeholder="candidate" value="<?php echo e($Voter->candidate); ?>" readonly>
+        </div>
+        <div class="col mb-3">
+            <label class="form-label">BSN</label>
+            <input type="text" name="BSN" class="form-control" placeholder="BSN" value="<?php echo e($Voter->BSN); ?>" readonly>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col mb-3">
+            <label class="form-label">Voter_code</label>
+            <input type="text" name="Voter_code" class="form-control" placeholder="Voter Code" value="<?php echo e($Voter->Voter_code); ?>" readonly>
+        </div>
+        <div class="col mb-3">
+            <label class="form-label">Description</label>
+            <textarea class="form-control" name="description" placeholder="Descriptoin" readonly><?php echo e($Voter->description); ?></textarea>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col mb-3">
+            <label class="form-label">Created At</label>
+            <input type="text" name="created_at" class="form-control" placeholder="Created At" value="<?php echo e($Voter->created_at); ?>" readonly>
+        </div>
+        <div class="col mb-3">
+            <label class="form-label">Updated At</label>
+            <input type="text" name="updated_at" class="form-control" placeholder="Updated At" value="<?php echo e($Voter->updated_at); ?>" readonly>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\bergs\Ivote\resources\views/Voter/show.blade.php ENDPATH**/ ?>
